7c857a51bccd37aea66cd58e5405372ff8520ced
VISHNU PREM <vishnuprem5152@gmail.com>
2024-08-22 18:10:32 +0530

    node modules reinstalled

--- src/pages/user/usercomponents/jwtAuthContext.jsx
+++ src/pages/user/usercomponents/jwtAuthContext.jsx
@@ -12,6 +12,8 @@ const getCookie = (name) => {
 
 const deleteCookie = (name) => {
   document.cookie = `${name}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
+  console.log("backend : ",BACKEND_SERVER);
+  
 };
 
 export const AuthContext = createContext();
