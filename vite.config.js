92c5d720707b9a7ac3fb10b956ec328769f2b7d8
VISHNU PREM <vishnuprem5152@gmail.com>
2024-08-22 15:52:09 +0530

    tailwind change

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-// tailwind.config.js
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    './src/**/*.{js,jsx,ts,tsx}', // Add paths to all of your pages and components
-    './public/index.html',         // If you are using Tailwind in your index.html
-    
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+});
+
 
