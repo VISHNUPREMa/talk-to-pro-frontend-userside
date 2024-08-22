9cf7811bd9b252b1e22bd4fa3b1dcf83fe82876a
VISHNU PREM <vishnuprem5152@gmail.com>
2024-08-22 15:40:28 +0530

    tailwind change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}', // Paths to all your components and pages
+    './public/index.html',         // Path to your HTML file if you're using Tailwind there
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ export default {
   content: [
     './src/**/*.{js,jsx,ts,tsx}', // Add paths to all of your pages and components
     './public/index.html',         // If you are using Tailwind in your index.html
+    
   ],
   theme: {
     extend: {},
